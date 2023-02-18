dea9a16478666175f1a5cbf1cfade17af711e11b
Joshua E. Roybal <u4ysev@gmail.com>
2023-02-17 23:15:36 -0600

    Delete job.lisp

--- job.lisp
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/clisp
-(load "graphs.fas")
-(defvar *G*)
-(setf *random-state* (make-random-state t))
-(setf *G* (random-graph (+ 3 (random 6 *random-state*))))
-(print-adjacency-list *G*)
-(write-dot-file *G*)
-(run-shell-command "dot -Tsvg graph.gv > graph.svg")
-(run-shell-command "geeqie graph.svg &")
