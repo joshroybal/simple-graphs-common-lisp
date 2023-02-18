849418fca6a53d268afd44f6b40c1e2f07a6271c
Joshua E. Roybal <u4ysev@gmail.com>
2023-02-17 23:17:52 -0600

    Add files via upload
    Made graphs into a package. Graph now works with the groups package so it can produce a graph of the partition by element orders of the multiplicative group of integers congruent mod n.

--- graphs.lisp
+++ graphs.lisp
@@ -1,3 +1,17 @@
+(require "groups.lisp")
+
+;;; Common Lisp graph theory package
+(defpackage :graphs
+  (:use :common-lisp)
+  (:import-from :groups :subgroups :orders-alist)
+  (:export :random-graph
+	   :group-orders-graph
+           :print-adjacency-list
+	   :print-adjacency-matrix
+	   :write-dot-file-group
+	   :write-dot-file))
+(in-package :graphs)
+
 (defun letter (n)
   (intern (string (code-char n))))
 
@@ -78,14 +92,32 @@
 ;;; car is node, cdr are adjacenct nodes
 (defun adjacency-list (g)
   (let ((v (first g)) (e (second g)))
-    (pairlis v (mapcar #'(lambda (x) (neighbors x e)) v))))
+    (reverse (pairlis v (mapcar #'(lambda (x) (neighbors x e)) v)))))
 
 (defun node-color (label color)
   (format nil "~&~S [fillcolor=~A]" (string label) color))
 
-(defun write-dot-file (g)
+(defun write-dot-file-group (g filename)
   (let ((edges (second g)))
-    (with-open-file (outfile "graph.gv"
+    (with-open-file (outfile filename
+			     :direction :output
+			     :if-exists :supersede
+			     :if-does-not-exist :create)
+      (progn
+	(format outfile "~&graph {")
+	(format outfile "~&label=\"Z/~dZ\"" (+ 1 (car (last (car g)))))
+	(format outfile "~&layout=circo;")
+	(format outfile "~&node [shape=circle height=0 width=0 margin=0 style=filled]")
+	(format outfile "~&aspect=0.618; pad=1.0;")
+	(dolist (edge edges)
+	  (format outfile " ~S -- ~S " (first edge) (second edge)))
+	(dolist (node (color g))
+	  (format outfile "~&~a [fillcolor=~a]" (car node) (cdr node)))	
+	(format outfile "}")))))
+
+(defun write-dot-file (g filename)
+  (let ((edges (second g)))
+    (with-open-file (outfile filename
 			     :direction :output
 			     :if-exists :supersede
 			     :if-does-not-exist :create)
@@ -135,7 +167,7 @@
 ;;; Welch-Powell algorithm.
 (defun color (g)
   (let ((v (mapcar #'car (ordered-degree-alist g)))
-	(colors '(red orange yellow green blue indigo violet))
+	(colors '(red orange yellow green blue indigo violet cyan brown maroon slategray gray darkgray lightgray white black))
 	(adjlis (adjacency-list g)))
     (do ((nodes v)
 	 (c colors (cdr c))
@@ -143,9 +175,54 @@
 	((null nodes) coloring)
       (let ((seq (non-adjacent-nodes (car nodes) nodes adjlis))
 	    (assigned nil))
-	(format t "~&~S" seq)
+	;(format t "~&~S" seq)
 	(dolist (item seq)
 	  (cond ((none-adjacent-p item assigned adjlis)
 		 (setf coloring (cons (cons item (car c)) coloring))
 		 (setf assigned (cons item assigned))
 		 (setf nodes (remove item nodes)))))))))
+
+;; (defun number-letter (n)
+;;   (intern (string (code-char (+ n 65)))))
+
+;; ;; (defun numbers-letters (lis)
+;; ;;   (let ((out nil))
+;; ;;     (dolist (s lis out)
+;; ;;       (progn
+;; ;; 	(setf s (mapcar #'number-symbol s))
+;; ;; 	(setf out (cons s out))))))
+
+;; (defun numbers-letters (lis)
+;;   (mapcar #'(lambda (x) (number-letter x)) lis))
+
+(defun filter-orders-alist (ele alis)
+  (mapcar #'car (remove-if-not #'(lambda (x) (equalp ele (cdr x))) alis)))
+
+;; (defun collect-orders (group)
+;;   "Collect elements of the same order into list partition of group."
+;;   (let ((divisors (groups::divisors (length (car group))))
+;; 	(res nil))
+;;     (dolist (d divisors res)
+;;       (setf res (cons (filter-orders-alist d (orders-alist group)) res)))))
+
+;;; as above but implicit rather than state setting
+(defun order-partition (group)
+  "Collect elements of the same order into list partition of group."
+  (do ((in (groups::divisors (length (car group))) (cdr in))
+       (out nil (cons (filter-orders-alist (car in) (orders-alist group)) out)))
+      ((null in) (remove nil out))))
+
+;;; probably overly complicated?
+(defun group-orders-edges (group)
+  "Edges are all elements sharing order within group."
+  (let ((partition (order-partition group)) (res nil))
+    (dolist (lis partition res)
+      (do ((remaining lis (cdr remaining)))
+	  ((null remaining) res)
+	(do ((in remaining (cdr in))
+	     (out nil (cons (list (car remaining) (cadr in)) out)))
+	    ((null (cdr in)) (setf res (append res out))))))
+    (stable-sort (stable-sort res #'< :key #'cadr) #'< :key #'car)))
+
+(defun group-orders-graph (group)
+  (list (car group) (group-orders-edges group)))
